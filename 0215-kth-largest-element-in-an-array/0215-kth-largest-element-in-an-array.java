ba9a923341c5216679bedf1ecfb87ae8662515d1
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2023-12-20 01:35:56 -0700

    Time: 78 ms (11.92%), Space: 57.9 MB (10.32%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -3,9 +3,10 @@ class Solution {
         PriorityQueue<Integer>minHeap = new PriorityQueue<>();
         for(int i=0; i < nums.length; i++) {
             minHeap.add(nums[i]);
-            if(minHeap.size() > k) {
+        }
+        
+        for(int i=0; i < nums.length - k; i++) {
                 minHeap.remove();
-            }
         }
         
         return minHeap.remove();
