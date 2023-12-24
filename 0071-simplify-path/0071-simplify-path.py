58e4f68f7d1512037416f9465a2349267d0782ce
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2023-12-23 20:56:48 -0700

    Time: 36 ms (81.80%), Space: 17.5 MB (5.44%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,35 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        ignore = "."
+        go_back = ".."
+        if path == "" or len(path) == 1:
+            return "/"
+        i = 0
+        stack = []
+        if path[0] == "/":
+            i += 1
+        while i < len(path):
+            current = ""
+            while i < len(path) and path[i] != "/":
+                current += path[i]
+                i += 1
+            if current  == "..":
+                if stack: 
+                    stack.pop()
+                    continue
+                else: continue
+            if current == ".":
+                continue
+            if current != "":
+                stack.append(current)
+            i += 1
+        print(stack)
+        if not stack:
+            return "/"
+        res = ""
+        for dir in stack:
+            res += ("/" + dir)
+        return res
+            
+        
+        
