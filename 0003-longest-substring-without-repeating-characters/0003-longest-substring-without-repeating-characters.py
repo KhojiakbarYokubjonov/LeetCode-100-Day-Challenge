841b23fccc7bf3dca2727b732c1256e483b41bff
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2023-12-25 22:10:57 -0700

    Time: 55 ms (81.65%), Space: 17.4 MB (6.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,9 +7,11 @@ class Solution:
            
             if start != end and s[end] in s[start:end]:
                 overall = max(overall, curr)
+                # updates the start position to the index right after the first instance of the duplicate value
                 start = pos[s[end]] + 1
+                # recalculates the current longest substring lenth
                 curr = end - start + 1
-                pos[s[end]] = end
+
             else:
                 curr += 1
             pos[s[end]] = end
