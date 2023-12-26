3dd445cb2165f7ee3c38250e55e8ea57b5454de1
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2023-12-26 00:56:46 -0700

    Time: 78 ms (93.61%), Space: 17.4 MB (6.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -7,12 +7,16 @@ class Solution:
         most_freq = 0
         
         for right in range(len(s)):
+            
+            # storing count in dictionary
             ch = s[right]
             if ch not in count: count[ch] = 0
             count[ch] += 1
+            
+            # keeps track of most frequent char
             most_freq = max(most_freq, count[ch])
             
-            while (right - left + 1 - most_freq) > k:
+            if (right - left + 1 - most_freq) > k:
                 count[s[left]] -= 1
                 left += 1
             res = max(res, right - left + 1)
