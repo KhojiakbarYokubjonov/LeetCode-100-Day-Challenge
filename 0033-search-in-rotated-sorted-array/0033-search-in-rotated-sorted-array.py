b23ad5484171b6e64cecc96880004cd730c6e889
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2023-12-28 18:16:47 -0700

    Time: 91 ms (6.87%), Space: 17.9 MB (5.20%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,6 +1,10 @@
 class Solution:
+    """
+    IDEA: first find the starting position of the sorted list; then just do the usual binary search
+    """
     def search(self, nums: List[int], target: int) -> int:
         
+        # find the 
         N = len(nums)
         l, r = 0, N-1
         
@@ -10,13 +14,13 @@ class Solution:
                 l = mid + 1
             else:
                 r = mid
-        pivot = l
+        offset = l
         
         l, r = 0, N-1
         
         while l <= r:
             mid = (l + r) // 2
-            actual_mid = (pivot + mid) % N
+            actual_mid = (offset + mid) % N
             if nums[actual_mid] > target:
                 r = mid - 1
             elif nums[actual_mid] < target:
