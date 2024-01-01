6afc3b94f8206cff6170f2c248091fccb7ff4c91
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-01 10:08:56 -0700

    Time: 67 ms (5.15%), Space: 17.3 MB (14.15%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,17 +5,29 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-       
-        def helper(head):
-            nonlocal n
-            if not head:
-                return head
-            head.next = helper(head.next)
-            n -= 1
-            if n == 0:
-                return head.next
-            
-        
-            
+        if not head:
             return head
-        return helper(head)
+       
+        def reverse(head):
+            if not head or not head.next:
+                return head
+            
+            new = reverse(head.next)
+            head.next.next = head
+            head.next = None
+            
+            return new
+        reverseH = reverse(head)
+        if reverseH and n == 1:
+            return reverse(reverseH.next)
+        curr = reverseH
+        while curr and curr.next:
+            if n-1 == 1:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+            n -= 1
+                
+        # print(reverseH)
+        
+        return reverse(reverseH)
