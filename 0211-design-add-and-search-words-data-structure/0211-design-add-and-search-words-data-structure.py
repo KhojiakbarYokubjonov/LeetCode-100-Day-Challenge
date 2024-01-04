3b6a9b966a8d6975e835a72c9f19a18bebe58755
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-04 13:45:25 -0700

    Time: 1605 ms (79.90%), Space: 52 MB (95.35%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,49 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.table = {'in':False}
+
+    def addWord(self, word: str) -> None:
+        last = self.table
+        
+        for ch in word:
+            if ch not in last:
+                last[ch] = {'in':False}
+            last = last[ch]
+        last['in'] = True
+        
+
+    def search(self, word: str) -> bool:
+        
+        def helper(word, dic):
+            if len(word) == 1 and word != '.':
+                if word in dic: return dic[word]['in']
+                return False
+        
+            last = dic
+            for i in range(len(word)):
+                if word[i] != '.':
+                    if word[i] not in last:
+                        return False
+                    last = last[word[i]]
+                        
+                else:
+                    # if i+1 >= len(word): 
+                    #     print(word, i+1, last['in'])
+                    #     return last['in']
+                    for ch in last:
+                        if ch != 'in':
+                            rest = "" if i+1 >= len(word) else word[i+1:]
+                            new = ch + rest
+                            if helper(new, last): return True
+                    return False
+            return last['in']
+        return helper(word, self.table)
+            
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
