d867f536f29b5083fc3487d270f89282a3b7d06b
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-09 22:46:57 -0700

    Time: 4237 ms (77.36%), Space: 17.5 MB (12.52%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,34 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def dfs(row, col, board, word):
+            if len(word) == 0:
+                # print("found")
+                return True
+            
+            if ( row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) ):
+                return False
+            
+            
+            
+            if word[0] != board[row][col]:
+                return False
+            
+            ch = board[row][col]
+            board[row][col] = "x"
+            res =   (dfs(row-1, col, board, word[1:]) or
+                    dfs(row+1, col, board, word[1:]) or
+                    dfs(row, col-1, board, word[1:]) or
+                    dfs(row, col+1, board, word[1:]))
+            board[row][col] = ch
+            return  res
+        
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                if board[row][col] == word[0] and dfs(row, col, board, word):
+                    return True
+        return False
+                    
+                
+            
+        
