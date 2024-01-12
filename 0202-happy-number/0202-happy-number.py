d19becc149b330d4b556f8514b57784b5ad93ac7
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-11 22:37:40 -0700

    Time: 33 ms (92.37%), Space: 17.3 MB (25.81%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -3,7 +3,10 @@ class Solution:
         seen = set()
         while num not in seen:
             seen.add(num)
-            squares = [int(n)**2 for n in str(num)]
+            squares = []
+            while num:
+                squares.append((num % 10)**2)
+                num //= 10
             num = sum(squares)
           
         return num == 1
