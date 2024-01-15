6cf62544de4cf9aa8d754bebce68bb30ee33c362
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-14 19:46:31 -0700

    Time: 97 ms (58.70%), Space: 19.3 MB (25.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,60 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        mapping = {}
-        roadmap = {}
-        indegree = {}
-        q = []
-        if prerequisites == []:
-            return True
+        adjList = collections.defaultdict(list)
         
-        for course, prereq in prerequisites:
-            if prereq not in mapping:
-                mapping[prereq] = set()
-            if course not in mapping:
-                mapping[course] = set()
-
-            mapping[course].add(prereq)
-            indegree[course] = 1 + indegree.get(course, 0)
-            if prereq not in indegree:
-                indegree[prereq] = 0
-        
-        for course, prereq in prerequisites:
-            if indegree[course] == 0 and course not in q:
-                q.append(course)
-            if indegree[prereq] == 0 and prereq not in q:
-                q.append(prereq)
-            if prereq not in roadmap:
-                roadmap[prereq] = set()
-            if course not in roadmap:
-                roadmap[course] = set()
-            roadmap[prereq].add(course)
+        for c, pre in prerequisites:
+            adjList[pre].append(c)
             
         
-        count = 0
+        def cycle(current, tracker, visited):
+            visited.add(current)
+            tracker.add(current)
+            for node in adjList[current]:
+                if node in tracker: # means there is a cycle
+                    return True
+                # before calling cycle(), check if we havent already visited this node before
+                elif node not in visited and cycle(node, tracker, visited):
+                    return True
+            tracker.remove(current)
+            return False
         
-        # print(mapping)
-        # print(roadmap)
-        # print(indegree)
-        while q:
-            course = q.pop()
-            # print(course)
-            if course in mapping:
-                mapping.pop(course)
-
-            for c in roadmap[course]:
-                indegree[c] -= 1
-                if indegree[c] == 0:
-                    q.append(c)
-            count += 1
-        
-        return count == numCourses or len(mapping) == 0
-            
-        
-"""
-
-4
-[[0,1],[0,2],[1,3],[3,0]]
-"""       
+        visited = set() # helps avoid visiting old nodes again
+        for c in range(numCourses):
+            tracker = set() # used to detect cycles
+            if c not in visited and cycle(c, tracker, visited):
+                return False
+        return True
         
         
         
