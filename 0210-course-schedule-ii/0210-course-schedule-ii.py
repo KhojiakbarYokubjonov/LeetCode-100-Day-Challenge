c8a404744bac956641fe8cac8d10d999f4fc10fa
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-14 20:58:38 -0700

    Time: 101 ms (46.56%), Space: 19.5 MB (18.97%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -5,27 +5,26 @@ class Solution:
         for c, pre in prerequisites:
             adjList[pre].append(c)
        
-        self.cyclic = False
+        # self.cyclic = False
         def dfs(current, tracker, visited, path):
             visited.add(current)
             tracker.add(current)
             for node in adjList[current]:
                 if node in tracker: # means there is a cycle
-                    self.cyclic = True
-                if node not in visited:
-                    dfs(node, tracker, visited, path)
+                    return True
+                if node not in visited and dfs(node, tracker, visited, path):
+                    return True
             path.append(current)
             tracker.remove(current)
+            return False
 
         path = []
         visited = set() # helps avoid visiting old nodes again
         
         for c in range(numCourses):
             tracker = set() # used to detect cycles
-            if c not in visited:
-                dfs(c, tracker, visited, path)
-        if self.cyclic:
-            return []
+            if c not in visited and dfs(c, tracker, visited, path):
+                return []
         path.reverse()
         return path
         
