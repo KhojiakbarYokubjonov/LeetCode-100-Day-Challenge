20ba0335e774bfc8645c0eb882c0060983dbe5a5
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-17 14:39:17 -0700

    Time: 3110 ms (21.00%), Space: 82.3 MB (73.59%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -3,6 +3,9 @@ class Solution:
     Idea: calculate all the edge weights and find MST cost using Prim's or kruskal's algorithms
     """
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        """
+        Solution using Kruskal's algorithm
+        """
         
         def find(v):
             if v != parent[v]:
@@ -45,6 +48,36 @@ class Solution:
         
         return cost
     
+    def solution2(self, points: List[List[int]]) -> int:
+        """
+        Solution using Prims algorithm
+        """
+        adjList = collections.defaultdict(list)
+        n = len(points)
+        
+        for i in range(len(points)):
+            x1, y1 = points[i][0], points[i][1]
+            for j in range(i+1, len(points)):
+                x2, y2 = points[j][0], points[j][1]
+                
+                distance = abs(x1 - x2) + abs(y1 - y2)
+                adjList[i].append((distance, j))
+                adjList[j].append((distance, i))
+        
+        cost = 0
+        visited = set()
+        heap = [(0, 0)] # dist, vertex
+        
+        while len(visited) < n:
+            d, vertex = heapq.heappop(heap)
+            if vertex in visited:
+                continue
+            visited.add(vertex)
+            cost += d
+            for node in adjList[vertex]:
+                if node[1] not in visited:
+                    heapq.heappush(heap, node)
+        return cost
     
             
         
