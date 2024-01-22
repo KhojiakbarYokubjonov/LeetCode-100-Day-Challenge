b5265fd42c41d1779d14c3f3b90e769b984e1e56
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-22 08:17:48 -0700

    Time: 40 ms (48.40%), Space: 16.5 MB (58.70%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,8 +1,12 @@
 class Solution:
+    """
+    Idea: same approach as one used for House Robber I. if you count nums[0], you can go upto nums[-2]. If you begin with nums[2],          you can go till the end.
+    """
     def rob(self, values: List[int]) -> int:
         N = len(values)
         if N == 1:
             return values[0]
+        
         def helper(nums):
             if len(nums) == 0: 
                 return 0
@@ -17,5 +21,5 @@ class Solution:
                 dp[i] = max(nums[i] + dp[i-2], dp[i-1])
             return dp[-1]
         
-        return max(helper(values[:N-1]), helper(values[1:N]))
+        return max(helper(values[:N-1]), helper(values[1:]))
         
