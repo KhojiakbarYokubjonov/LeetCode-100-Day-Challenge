8ea2173bac76e90cf6fe5f31db7ffc11336429b8
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-21 22:34:09 -0700

    Time: 32 ms (88.46%), Space: 16.5 MB (61.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,20 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        N = len(nums)
-        if len(nums) == 0:
-            return 0
-        if N < 3:
-            return max(nums)
-        dp = [0] * N
         
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
         
-        for i in range(2, N):
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
-        
-        # print(dp)
-        return dp[-1]
+        dp = [-1] * len(nums)
+        def recursiveSol(i):
+            if i >= len(nums):
+                return 0
+            if i == len(nums) - 1:
+                return nums[i]
+            if dp[i] != -1:
+                return dp[i]
+            
+            dp[i] = max(nums[i] + recursiveSol(i+2), recursiveSol(i+1))
+            return dp[i]
+        res = recursiveSol(0)
+        return res
         
             
                 
