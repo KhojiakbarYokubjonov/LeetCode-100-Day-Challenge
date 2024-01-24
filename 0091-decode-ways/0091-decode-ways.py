387379820eb59165e4fa06c9321c03c933e8660b
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-24 14:31:56 -0700

    Time: 33 ms (82.51%), Space: 16.6 MB (88.70%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        """
+        dp = [0,1,0,0]
+        
+        2nd ch
+        dp = [0,1,2,0]
+        
+        3rd ch
+        dp = [0,1,2,3]
+        
+        
+        """
+        
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+        
+        for i in range(2, len(s) + 1):
+            currentD = int(s[i-1])
+            twoD = int(s[i-2:i])
+            if currentD > 0:
+                dp[i] += dp[i-1]
+            if 10 <= twoD <= 26:
+                dp[i] += dp[i-2]
+        print(dp)
+        return dp[len(s)]
+        
