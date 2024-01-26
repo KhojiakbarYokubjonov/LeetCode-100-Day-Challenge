33f05994e5fc13dcd8120e451be13ac47c32fabb
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-26 10:30:55 -0700

    Time: 65 ms (95.53%), Space: 17.1 MB (65.21%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -16,3 +16,17 @@ class Solution:
                 mindp = min(maxcopy * nums[i], nums[i])
             maxprod = max(maxprod, maxdp)
         return maxprod
+    
+    def solutionWithArrays(nums):
+        mindp = [0] * len(nums)
+        maxdp = [0] * len(nums)
+        mindp[0] = maxdp[0] = nums[0]
+        
+        for i in range(1, len(nums)):
+            if nums[i] > 0:
+                maxdp[i] = max(maxdp[i-1] * nums[i], nums[i])
+                mindp[i] = min(mindp[i-1] * nums[i], nums[i])
+            else:
+                maxdp[i] = max(mindp[i-1] * nums[i], nums[i])
+                mindp[i] = min(maxdp[i-1] * nums[i], nums[i])
+        return max(maxdp)
