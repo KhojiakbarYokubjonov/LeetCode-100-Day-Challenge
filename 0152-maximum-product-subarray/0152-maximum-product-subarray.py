b31555adcdb79ba2d5028ef6192f1a381c6b338e
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-26 10:30:03 -0700

    Time: 67 ms (92.06%), Space: 17 MB (71.16%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,15 +3,16 @@ class Solution:
     Idea: have 2 dp arrays: maxdp and mindp
     """
     def maxProduct(self, nums: List[int]) -> int:
-        mindp = [0] * len(nums)
-        maxdp = [0] * len(nums)
-        mindp[0] = maxdp[0] = nums[0]
+  
+        maxprod = mindp = maxdp = nums[0]
         
         for i in range(1, len(nums)):
             if nums[i] > 0:
-                maxdp[i] = max(maxdp[i-1] * nums[i], nums[i])
-                mindp[i] = min(mindp[i-1] * nums[i], nums[i])
+                maxdp = max(maxdp * nums[i], nums[i])
+                mindp = min(mindp * nums[i], nums[i])
             else:
-                maxdp[i] = max(mindp[i-1] * nums[i], nums[i])
-                mindp[i] = min(maxdp[i-1] * nums[i], nums[i])
-        return max(maxdp)
+                maxcopy = maxdp
+                maxdp = max(mindp * nums[i], nums[i])
+                mindp = min(maxcopy * nums[i], nums[i])
+            maxprod = max(maxprod, maxdp)
+        return maxprod
