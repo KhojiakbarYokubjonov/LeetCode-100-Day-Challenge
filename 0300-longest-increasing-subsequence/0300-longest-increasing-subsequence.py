4344491bc7047e6da3a7f8e4c837fd575c089c1b
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-01-26 22:56:32 -0700

    Time: 1810 ms (40.80%), Space: 16.9 MB (86.61%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -8,7 +8,7 @@ class Solution:
             for j in range(i):
                 if nums[j] < nums[i]:
                     dp[i] = max(dp[i], dp[j]+1)
-                    maxlen = max(maxlen, dp[i])
+            maxlen = max(maxlen, dp[i])
         return maxlen
                 
             
