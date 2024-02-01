3483704a6f3eaf00065ad19029b2836aee0c0cbc
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-02-01 10:42:04 -0700

    Time: 38 ms (55.89%), Space: 16.7 MB (59.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,18 +5,24 @@ class Solution:
         
         def dfs(x, y):
             if x < 0 or y < 0 or x >= m or y >= n:
-                return 0
+                return
             if x == m-1 and y == n-1:
                 dp[x][y] = 1
-                return 1
+                return
             if dp[x][y] != -1:
-                return dp[x][y]
-            dp[x][y] = dfs(x+1,y) + dfs(x,y+1)
+                return
             
-            return dp[x][y]
+            dfs(x+1,y)
+            dfs(x,y+1)
             
+            paths = 0
+            if x+1 < m:
+                paths += dp[x+1][y]
+            if y+1 < n:
+                paths += dp[x][y+1]
+            dp[x][y] = paths
         
         dfs(0, 0)
-        # print(dp)
+        print(dp)
         return dp[0][0]
         
