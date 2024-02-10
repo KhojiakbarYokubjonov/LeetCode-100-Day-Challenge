e773ec1162a5f9c005c0d3a65116073f27675d00
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-02-10 16:54:38 -0700

    Time: 76 ms (41.38%), Space: 23.4 MB (57.25%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        countofones = [0] * (n+1)
+        countofones[0] = 0
+        
+        offset = 1
+        
+        for i in range(1, n+1):
+            print(offset)
+            if i == offset * 2:
+                offset = i
+            countofones[i] = 1 + countofones[i - offset]
+        return countofones
+   
+                
+             
+        
+        
