a9da4e49c23e9af476d18504609f24e1a6b336a5
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-02-10 16:56:39 -0700

    Time: 50 ms (97.50%), Space: 23.1 MB (86.36%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,5 +1,15 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        count = [0] * (n + 1)
+        for i in range(n+1):
+            count[i] = count[i >> 1] + i % 2
+        return count
+    
+    
+    
+    
+    
+    def offsetSolution(n):
         countofones = [0] * (n+1)
         countofones[0] = 0
         
