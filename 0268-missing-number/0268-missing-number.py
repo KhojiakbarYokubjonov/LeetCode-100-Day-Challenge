14a8f0213e58cae37c48e45b914a6388ca20ffb7
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-02-10 19:25:42 -0700

    Time: 108 ms (82.57%), Space: 17.8 MB (85.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        missingnum = 0
+        N = len(nums)
+        for n in range(1, N+1):
+            missingnum ^= n
+            missingnum ^= nums[n-1]
+        return missingnum
+        
+        
+        
+        
+        
+        
+    def solutionOne(nums):
         N = len(nums)
         total = int(N*(N+ 1) / 2)
         for n in nums:
