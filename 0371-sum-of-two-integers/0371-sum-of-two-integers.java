05ec0642a975813549b6c228b1581d79ab44a073
Khojiakbar Yokubjonov <khyokubjonov@gmail.com>
2024-02-10 20:07:22 -0700

    Time: 0 ms (100.00%), Space: 40 MB (81.83%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,10 +1,10 @@
 class Solution {
     public int getSum(int a, int b) {
         // repeat the steps until the carry value is 0
-        while(b != 0) { 
+        while(b != 0) {
             int carrybits = (a & b);
             a = a ^ b; // add and store sum on a (this ignores carrybits)
-            b = carrybits << 1;
+            b = carrybits << 1; // now b represents the carry value
         }
         return a;
     }
